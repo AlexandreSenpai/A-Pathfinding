1c06fb940235a57e8b6ce69616ef7f4b619e7411
Alexandre Brito <alexandre_brito_de_souza_ramos_hvar@whirlpool.com>
2024-01-05 03:22:53 -0300

    refactor: removed draw method from gridc

--- grid.py
+++ grid.py
@@ -9,42 +9,6 @@ class Grid:
         self.width = width
         self.height = height
         self.__matrix: list[list[Node]] = []
-
-    def draw(self, selected_nodes: list[Node], processed: list[Node], done=False) -> None:
-        class COLORS(enum.Enum):
-            HEADER = '\033[95m'
-            OKBLUE = '\033[94m'
-            OKCYAN = '\033[96m'
-            OKGREEN = '\033[92m'
-            WARNING = '\033[93m'
-            FAIL = '\033[91m'
-            ENDC = '\033[0m'
-            BOLD = '\033[1m'
-            UNDERLINE = '\033[4m'
-
-        for x in range(self.width):
-            line = ''
-            for y in range(self.height):
-                node = self.get_node_at(x, y)
-
-                if done and selected_nodes:
-                    if node in selected_nodes:
-                        line += f'{COLORS.BOLD.value}{COLORS.OKGREEN.value}[{str(node)}]{COLORS.ENDC.value}'
-                    else:
-                        line += f'{COLORS.BOLD.value}{COLORS.WARNING.value}[{str(node)}]{COLORS.ENDC.value}'
-                    continue
-
-                if node is not None and node not in processed and node not in selected_nodes:
-                    line += f'[{COLORS.WARNING.value}F={node.F}{COLORS.ENDC.value} G={node.G} H={node.H}]'
-                
-                if node in processed:
-                    line += f'{COLORS.OKBLUE.value}[F={node.F} G={node.G} H={node.H}]{COLORS.ENDC.value}'
-                
-                if node in selected_nodes:
-                    line += f'{COLORS.OKGREEN.value}[F={node.F} G={node.G} H={node.H}]{COLORS.ENDC.value}'
-                
-            print(line, flush=True)
-        print('\n')
     
     def make(self) -> None:
         for x in range(self.width):
